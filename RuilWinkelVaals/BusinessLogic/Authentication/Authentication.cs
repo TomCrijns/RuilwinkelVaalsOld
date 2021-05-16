183f7e63b1c5818e58c5fd4721dad2be32f1733d
Tom Crijns <1816802crijns@zuyd.nl>
2021-05-16 11:56:26 +0200

    Login feature completed

---
+++ RuilWinkelVaals/BusinessLogic/Authentication/Authentication.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using RuilWinkelVaals;
+
+
+namespace RuilWinkelVaals.BusinessLogic.Authentication
+{
+    public class Authentication
+    {
+        public string password { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="typedUserName">Username filled in by user</param>
+        /// <param name="userName">Username that's stored in database</param>
+        /// <param name="password">Password filled in by user</param>
+        /// <param name="hash">Password hash stored in database</param>
+        /// <param name="salt">Salt that belongs to stored password</param>
+        /// <returns></returns>
+        public static bool AuthenticateUser(string typedUserName, string userName, string password, string hash, string salt)
+        {
+            //Turn filled in password to hash
+            var saltBytes = Convert.FromBase64String(salt);
+            var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 10000);
+
+            //Check if there's a match between stored password and filled in password
+            bool isPasswordMatch = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256)) == hash;
+
+            if (userName == typedUserName && isPasswordMatch)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ RuilWinkelVaals/BusinessLogic/Authentication/HashSalt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace RuilWinkelVaals.BusinessLogic.Authentication
+{
+    public class HashSalt
+    {
+        public string hash { get; set; }
+        public string salt { get; set; }
+
+        /// <summary>
+        /// This method generates a hash and a salt for the filled in password which provides extra security
+        /// </summary>
+        /// <param name="size">Length is salt</param>
+        /// <param name="password">Password filled in by user</param>
+        /// <returns></returns>
+        public static HashSalt GenerateHashSalt(int size, string password)
+        {
+            //Generate salt
+            var saltBytes = new byte[size];
+            var provider = new RNGCryptoServiceProvider();
+            provider.GetNonZeroBytes(saltBytes);
+            var salt = Convert.ToBase64String(saltBytes);
+
+            //Generate hash
+            var rfc28898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 10000);
+            var hashPassword = Convert.ToBase64String(rfc28898DeriveBytes.GetBytes(256));
+
+            //Store hash & salt in hashSalt object that gets returned to requester
+            HashSalt hashSalt = new HashSalt { hash = hashPassword, salt = salt };
+            return hashSalt;
+        }
+    }
+}
