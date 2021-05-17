bf9a1a8a93c25eedf54a7dfe580dd8fd8ad827e8
Tom Crijns <1816802crijns@zuyd.nl>
2021-05-17 19:17:10 +0200

    bugfixje

--- RuilWinkelVaals/Controllers/HomeController.cs
+++ RuilWinkelVaals/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace RuilWinkelVaals.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
