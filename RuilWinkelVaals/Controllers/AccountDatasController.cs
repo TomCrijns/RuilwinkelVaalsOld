2edc82a4893640a0699d93745c436b4b5bc5fdb7
Jek <jekkowdev@gmail.com>
2021-05-17 09:06:32 +0200

    Registratie functions

---
+++ RuilWinkelVaals/Controllers/AccountDatasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RuilWinkelVaals.Models;
+
+namespace RuilWinkelVaals.Controllers
+{
+    public class AccountDatasController : Controller
+    {
+        private UserDataModel db = new UserDataModel();
+
+        // GET: AccountDatas
+        public ActionResult Index()
+        {
+            var accountData = db.AccountData.Include(a => a.ProfileData);
+            return View(accountData.ToList());
+        }
+
+        // GET: AccountDatas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountData accountData = db.AccountData.Find(id);
+            if (accountData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountData);
+        }
+
+        // GET: AccountDatas/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProfileId = new SelectList(db.ProfileData, "Id", "Email");
+            return View();
+        }
+
+        // POST: AccountDatas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ProfileId,Hash,Salt,Blocked,DateBlocked")] AccountData accountData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AccountData.Add(accountData);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProfileId = new SelectList(db.ProfileData, "Id", "Email", accountData.ProfileId);
+            return View(accountData);
+        }
+
+        // GET: AccountDatas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountData accountData = db.AccountData.Find(id);
+            if (accountData == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProfileId = new SelectList(db.ProfileData, "Id", "Email", accountData.ProfileId);
+            return View(accountData);
+        }
+
+        // POST: AccountDatas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ProfileId,Hash,Salt,Blocked,DateBlocked")] AccountData accountData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accountData).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProfileId = new SelectList(db.ProfileData, "Id", "Email", accountData.ProfileId);
+            return View(accountData);
+        }
+
+        // GET: AccountDatas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountData accountData = db.AccountData.Find(id);
+            if (accountData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accountData);
+        }
+
+        // POST: AccountDatas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AccountData accountData = db.AccountData.Find(id);
+            db.AccountData.Remove(accountData);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RuilWinkelVaals/Controllers/RegisterController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RuilWinkelVaals.BusinessLogic.Authentication;
+using RuilWinkelVaals.Models;
+using RuilWinkelVaals.ViewModel;
+
+namespace RuilWinkelVaals.Controllers
+{
+    public class RegisterController : Controller
+    {
+        UserDataModel db = new UserDataModel();
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register([Bind(Include = "Email, Password, ValidationPassword, Voornaam, Achternaam, Straat, Huisnummer, Woonplaats, Postcode, Geboortedatum, Zakelijk")]Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Try to get an Profile where email is similar to the userinput
+                var profileExists = db.ProfileData.Where(user => user.Email == model.Email).FirstOrDefault();
+                if(profileExists == null) //If there is not an registered user with the given Email
+                {
+                    var ProfileAge = GetAge(Convert.ToDateTime(model.Geboortedatum).Date);
+                    if(ProfileAge >= 16) //Customer is old enough to register
+                    {
+                        if(model.Password == model.ValidationPassword) //Given passwords are equal
+                        {
+                            ProfileData newProfile = new ProfileData() //Create new ProfileObject
+                            {
+                                Email = model.Email,
+                                Voornaam = model.Voornaam,
+                                Achternaam = model.Achternaam,
+                                Straat = model.Straat,
+                                Huisnummer = model.Huisnummer,
+                                Woonplaats = model.Woonplaats,
+                                Postcode = model.Postcode,
+                                Geboortedatum = model.Geboortedatum,
+                                DateCreated = DateTime.Today.Date,
+                            };
+
+                            //Save profile to DB
+                            db.ProfileData.Add(newProfile);
+                            db.SaveChanges();
+
+                            //Get ProfileId for Foreign relation
+                            var ProfileId = db.ProfileData.Where(profile => profile.Email == model.Email).FirstOrDefault();
+                            //Create new Account with relation to ProfileData
+                            HashSalt hashSalt = HashSalt.GenerateHashSalt(16, model.Password);
+                            AccountData newAccount = new AccountData()
+                            {
+                                ProfileId = ProfileId.Id,
+                                Hash = hashSalt.hash,
+                                Salt = hashSalt.salt,
+                            };
+                            db.AccountData.Add(newAccount);
+                            db.SaveChanges();
+                            return RedirectToAction("About", "Home");
+
+                        }
+                        else //Passwords are not equal
+                        {
+                            ModelState.AddModelError("ValidationError", "De gegeven wachtwoorden komen niet overeen met elkaar.");
+                            return View();
+                        }
+
+                    }
+                    else //Customer is not old enough to register
+                    {
+                        ModelState.AddModelError("ValidationError", "U dient minimaal 16jaar te zijn om te registreren.");
+                        return View();
+                    }
+                }
+                else //If there is an user with the given Email
+                {
+                    ModelState.AddModelError("ValidationError", "Er bestaat al een account met dit Email adres.");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public int GetAge(DateTime bornDate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - bornDate.Year;
+            if (bornDate > today.AddYears(-age))
+                age--;
+            return age;
+        }
+    }
+}
