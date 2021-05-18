63090209ea57914ab49d9bddd560fa905620851f
Tom Crijns <1816802crijns@zuyd.nl>
2021-05-18 12:10:29 +0200

    test

--- RuilWinkelVaals/Controllers/HomeController.cs
+++ RuilWinkelVaals/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace RuilWinkelVaals.Controllers
         {
             return View();
         }
-
+        //Get About
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
